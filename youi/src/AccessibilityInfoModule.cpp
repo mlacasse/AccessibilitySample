755d044cec4bfa1fa9cfe337c0f3f9379ac3decf
Marc Lacasse <marc.lacasse@mac.com>
2020-05-07 15:13:24 -0400

    Added AccessibilityInfo

---
+++ youi/src/AccessibilityInfoModule.cpp
@@ -0,0 +1,23 @@
+#include "AccessibilityInfoModule.h"
+
+#include <youireact/YiReactNativeView.h>
+
+using namespace yi::react;
+
+YI_RN_INSTANTIATE_MODULE(AccessibilityInfoModule);
+
+YI_RN_REGISTER_MODULE(AccessibilityInfoModule);
+
+AccessibilityInfoModule::AccessibilityInfoModule()
+{}
+
+#if !defined(YI_ANDROID) && !defined(YI_IOS) && !defined(YI_TVOS)
+YI_RN_DEFINE_EXPORT_METHOD(AccessibilityInfoModule, get)(Callback successCallback, Callback failedCallback)
+{
+    folly::dynamic errorInfo = folly::dynamic::object;
+    
+    errorInfo["message"] = "React Native AccessibilityInfoModule is not supported on this platform.";
+    
+    failedCallback({ ToDynamic(errorInfo) });
+}
+#endif
