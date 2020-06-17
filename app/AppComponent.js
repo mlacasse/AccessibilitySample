f679a9dafed447ad4ab8ff03a8e2a8929c1c7349
Marc Lacasse <marc.lacasse@mac.com>
2020-06-17 13:04:12 -0400

    Added slider

--- app/AppComponent.js
+++ app/AppComponent.js
@@ -1,5 +1,6 @@
 import React, { PureComponent, Fragment } from 'react';
-import { AppState, FlatList, NativeModules, ScrollView, Text, View } from 'react-native';
+import { AppState, FlatList, NativeModules, ScrollView, Slider, Text, View } from 'react-native';
+import { SpeechSynthesizer } from '@youi/react-native-youi';
 import { connect } from 'react-redux';
 
 import Photo from './Photo';
@@ -35,6 +36,20 @@ class AppComponent extends PureComponent {
   componentWillUnmount() {
     AppState.removeEventListener('change', this._handleAppStateChange);
   }
+
+  _onAccessibilityAction = (event) => {
+    const { actionName } = event.nativeEvent;
+
+    let utterance = '';
+
+    switch(actionName) {
+      default:
+        utterance = `Accessibility action ${actionName}`;
+        break;
+    };
+
+    SpeechSynthesizer.speak(utterance);
+  };
 
   _handleAppStateChange = (newAppState) => {
     if (AccessibilityInfo && newAppState === 'active') {
@@ -103,8 +118,24 @@ class AppComponent extends PureComponent {
         }}>
           <Text style={{ fontSize: 14, color: 'black' }}>Accessibility Sample</Text>
         </View>
+        <View style={{
+          alignItems: 'center',
+          padding: 5,
+          backgroundColor: 'white',
+        }}>
+          <Slider
+            accessible
+            accessibilityLabel={'This is a slider '}
+            accessibilityRole={'adjustable'}
+            accessibilityActions={[{ name: 'increment' }, { name: 'decrement' }]}
+            onAccessibilityAction={this._onAccessibilityAction}
+            maximumValue={100}
+            value={50}
+            step={1}
+          />
+        </View>
         <ScrollView style={{ flex: 1 }}>
-          <FlatList horizontal keyExtractor={item => "" + item.id} data={complexes} renderItem={this._renderComplex}/>
+          <FlatList horizontal keyExtractor={item => "" + item.id} data={complexes} renderItem={this._renderComplex} />
           <FlatList horizontal keyExtractor={item => "" + item.id} data={posters} renderItem={this._renderPoster} />
           <FlatList horizontal keyExtractor={item => "" + item.id} data={landscapes} renderItem={this._renderLandscape} />
           <FlatList horizontal keyExtractor={item => "" + item.id} data={blurry} renderItem={this._renderBlurry} />
